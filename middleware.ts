d3543f906bb5330f6442555149d9349bfa6fbc03
osh3276 <osh3276@gmail.com>
2026-06-23 10:16:48 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -48,6 +48,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+		"/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\.[a-zA-Z0-9]+$).*)",
 	],
 };
